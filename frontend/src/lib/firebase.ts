a75d83d06645d530421630352c6b6844eb53a739
DTCode <daithien2004@gmail.com>
2025-10-06 01:05:28 +0700

    fix: firebase config

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCWdLQUVm3NLnluCpjpICXiCo-Prz3mVQI',
-  authDomain: 'socialbook-19b8f.firebaseapp.com',
-  projectId: 'socialbook-19b8f',
-  storageBucket: 'socialbook-19b8f.firebasestorage.app',
-  messagingSenderId: '640572357065',
-  appId: '1:640572357065:web:f15b8fb52b825c4db05018',
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
